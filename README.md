80fdd74dde8056525d46620d71d3fe0633ede192
Aditya Srivastava <yashsri421@gmail.com>
2020-12-11 14:08:30 +0530

    Use clang-format-diff for formatting, add steps to run script

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ apply-patch
 
 The applypatch script can automatically apply patches into a
 test branch and run clang-format on the patch context.
+
+### Steps to run the script:
+
+- Copy this script in `scripts/` directory
+
+- Run `perl ./scripts/applypatch.pl --clang-format {patchfile}` (replace 'patchfile' with your patchfile).<br/>
+- This will format the changes made using clang-format and apply it on top of a new branch("test-{your_current_branch}").<br/>
+You can provide your own branch name using `--branch={branch_name}` option.
+- Also a modified patchfile will be created alongside your pathfile with suffix `.EXPERIMENTAL-clang_format-fixes`.
+
+### Pre-requisite for this script:
+
+- The patchfile should be applicable to the recent state of the branch.
+- This script is dependent on `clang-format-8` to function properly.<br/>
+Any version less than 8 is dependent on python2 and the latest Terminals(atleast for Ubuntu 18.04) are dependent on python3. So changing Python version for running the script may not be a great option.
--- applypatch.pl
+++ applypatch.pl
@@ -13,10 +13,15 @@ use Getopt::Long qw(:config no_auto_abbrev);
 
 my $root = '../';
 my $cur_path = fastgetcwd() . '/';
-my $git_clang_format = 'git clang-format';
-my $git_clang_format_file = '/usr/bin/git-clang-format';
+my $clang_format_diff = 'clang-format-diff-8 -p1 -i';
+my $clang_format_diff_file = '/usr/bin/clang-format-diff-8';
 my $use_clang_format = 0;
-my @patch_files = ();
+# my @patch_files = ();
+
+# replacing array of patchfiles with only one patchfile, as most probably only one patchfile might be run to get the modified diff
+# Ofcourse, it can be extended for multiple patchfiles as well, just multiple modified patches will be created
+my $patchfile;
+
 my @cmd_args = ();
 my $orig_branch = '';
 my $test_branch = '';
@@ -27,127 +32,133 @@ my $V = '0.1';
 my $P = $0;
 
 if (!GetOptions(
-        'branch=s' => \$test_branch,
-        'clang-format' => \$use_clang_format,
-        'h|help|usage' => \$help,
-        )) {
-    die "$P: invalid argument - use --help if necessary\n";
+		'branch=s' => \$test_branch,
+		'clang-format' => \$use_clang_format,
+		'h|help|usage' => \$help,
+		)) {
+	die "$P: invalid argument - use --help if necessary\n";
 }
 
 foreach (@ARGV) {
-    push(@cmd_args, $_);
-    if ($_ =~ /.*\.patch$/) {
-        push(@patch_files, $_);
-    }
+	push(@cmd_args, $_);
+	if ($_ =~ /.*\.patch$/) {
+		# push(@patch_files, $_);
+                $patchfile = $_;
+	}
 }
 
-if (@cmd_args == 0 || $help != 0) {
-    usage();
-    exit 0;
+if (scalar @cmd_args == 0 || $help != 0) {
+	usage();
+	exit 0;
 }
 
 my %VCS_cmds_git = (
-    "apply_patch" => "git am \$patch",
-    "get_current_branch" => "git branch --show-current",
-    "get_short_revision" => "git rev-parse --short HEAD",
-    "check_branch" => "git show-ref refs/heads/\$branch",
-    "switch_branch" => "git checkout \$branch",
-    "switch_branch_new" => "git checkout -b \$branch"
+	"apply_patch" => "git am \$patch",
+	"get_current_branch" => "git rev-parse --abbrev-ref HEAD",
+	"get_short_revision" => "git rev-parse --short HEAD",
+	"check_branch" => "git show-ref refs/heads/\$branch",
+	"switch_branch" => "git checkout \$branch",
+	"switch_branch_new" => "git checkout -b \$branch"
 );
 
 sub usage {
-    print <<EOT;
+	print <<EOT;
 usage: $P [options] patchfile
 version: $V
-
 Options:
 --branch=<branch-name> => test branch name
 --help => show this help information
 EOT
 }
 
+sub install_clang_format {
+        print <<EOT;
+clang-format-diff-8 not found
+You can install it using:
+apt-get install clang-format-diff-8
+EOT
+}
+
 sub git_execute_cmd {
-    my ($cmd) = @_;
-    my @lines = ();
+	my ($cmd) = @_;
+	my @lines = ();
 
-    my $output = `$cmd`;
-    $output =~ s/^\s*//gm;
-    @lines = split("\n", $output);
+	my $output = `$cmd`;
+	$output =~ s/^\s*//gm;
+	@lines = split("\n", $output);
 
-    return @lines;
+	return @lines;
 }
 
 sub git_get_branch {
-    my $cmd = $VCS_cmds_git{"get_current_branch"};
-    my @lines = git_execute_cmd($cmd);
+	my $cmd = $VCS_cmds_git{"get_current_branch"};
+	my @lines = git_execute_cmd($cmd);
 
-    return $lines[0];
+	return $lines[0];
 }
 
 sub git_get_revision {
-    my $cmd = $VCS_cmds_git{"get_short_revision"};
-    my @lines = git_execute_cmd($cmd);
+	my $cmd = $VCS_cmds_git{"get_short_revision"};
+	my @lines = git_execute_cmd($cmd);
 
-    return $lines[0];
+	return $lines[0];
 }
 
 sub git_change_branch {
-    my ($branch) = @_;
-    my @lines = ();
-    my $cmd = '';
-    
-    $cmd = $VCS_cmds_git{"check_branch"};
-    $cmd =~ s/(\$\w+)/$1/eeg;
-    @lines = git_execute_cmd($cmd);
-    
-    if (@lines) {
-        $cmd = $VCS_cmds_git{"switch_branch"};
-    } else {
-        $cmd = $VCS_cmds_git{"switch_branch_new"};
-    }
-    $cmd =~s/(\$\w+)/$1/eeg;
-    console_out($cmd);
-    git_execute_cmd($cmd);
+	my ($branch) = @_;
+	my @lines = ();
+	my $cmd = '';
+	
+	$cmd = $VCS_cmds_git{"check_branch"};
+	$cmd =~ s/(\$\w+)/$1/eeg;
+	@lines = git_execute_cmd($cmd);
+	
+	if (@lines) {
+		$cmd = $VCS_cmds_git{"switch_branch"};
+	} else {
+		$cmd = $VCS_cmds_git{"switch_branch_new"};
+	}
+	$cmd =~s/(\$\w+)/$1/eeg;
+	console_out($cmd);
+	git_execute_cmd($cmd);
 }
 
 sub console_out {
-    my ($out) = @_;
-    print GREEN . $out . RESET . "\n";
+	my ($out) = @_;
+	print GREEN . $out . RESET . "\n";
 }
 
 sub run_clang_format {
-    if (! -e $git_clang_format_file) {
-        die "$P: clang-format not found\n";
-    }
-    my $cmd = "$git_clang_format --diff $start_commit..$end_commit";
-    console_out($cmd);
-    my @lines = git_execute_cmd($cmd);
+	if (! -e $clang_format_diff_file) {
+		die "$P: ". install_clang_format();
+	}
+        my $cmd = `cat $patchfile | $clang_format_diff`;
+        $cmd = `git add -u`;
+        $cmd = `git commit --amend --no-edit`;
+	my $tmp_patch = `git format-patch -o /tmp/clang-format-diff HEAD~1`;
+	$tmp_patch =~ s/\s*$//;
+	my $new_patchfile = "$patchfile.EXPERIMENTAL-clang_format-fixes";
+	$cmd = `cp $tmp_patch $new_patchfile`; # creates clang-format modified diff
+	console_out("Clang formatted patchfile generated at: $new_patchfile");
 }
 
 sub apply {
-    my @patches = @_;
-    $start_commit = git_get_revision();
-
-    foreach (@patches) {
-        my $patch = $_;
-        my $cmd = $VCS_cmds_git{"apply_patch"};
-        $cmd =~ s/(\$\w+)/$1/eeg;
-        console_out($cmd);
-        git_execute_cmd($cmd);
-    }
-    $end_commit = git_get_revision();
+	my ($patchfile) = @_;
+	my $cmd = $VCS_cmds_git{"apply_patch"} . $patchfile;
+	console_out($cmd);
+	git_execute_cmd($cmd);
 }
 
 $orig_branch = git_get_branch();
 if ($test_branch eq '') {
-    $test_branch = "test-" . $orig_branch;
+	$test_branch = "test-" . $orig_branch;
 }
 
 git_change_branch($test_branch);
 
-apply(@patch_files);
+apply($patchfile);
 if ($use_clang_format) {
-    run_clang_format();
+	run_clang_format();
 }
 
 git_change_branch($orig_branch);
